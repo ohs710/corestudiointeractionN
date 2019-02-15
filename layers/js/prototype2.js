e81138aac18af4515ef8a62f3ebc75f9e33d6d75
Sujin <ohs710@newschool.edu>
2019-02-15 00:59:06 -0500

    g
    g

---
+++ layers/js/prototype2.js
@@ -0,0 +1,15 @@
+$( "button" ).click(function() {
+  $( "bedframe" ).show( "slow" );
+});
+
+$( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+  } );
