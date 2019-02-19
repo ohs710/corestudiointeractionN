dcb3d0ac0da3a8ee246d166d128c2e1fd534a5f7
Sujin <ohs710@newschool.edu>
2019-02-18 19:59:18 -0500

    f
    f

--- layers/js/prototype2.js
+++ layers/js/prototype2.js
@@ -2,7 +2,9 @@ function myFunction (){
   var x = document.getElementById("bedframe").onclick=function();
 
 $(document).ready(function(){
-  $("myButton").click(function(){
-    $("#bedframe").toggle();
+  $("button").click(function(){
+    $("p").toggle();
   });
 });
+
+
