a90c73489eff7e614caeafb2799d083c73369470
Sujin <ohs710@newschool.edu>
2019-02-21 00:07:20 -0500

    sad
    sad

--- layers/js/prototype2.js
+++ layers/js/prototype2.js
@@ -1,10 +1,5 @@
-function myFunction (){
-  var x = document.getElementById("bedframe").onclick=function();
-
-$(document).ready(function(){
-  $("button").click(function(){
-    $("p").toggle();
-  });
-});
+ $( function() {
+    $( "#draggable" ).draggable();
+  } );
 
 
