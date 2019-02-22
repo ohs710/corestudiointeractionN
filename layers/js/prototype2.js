8edb87e0a86511942dfe8b8145375e6315392bfd
Sujin <ohs710@newschool.edu>
2019-02-22 00:04:49 -0500

    f
    faef

--- layers/js/prototype2.js
+++ layers/js/prototype2.js
@@ -16,3 +16,24 @@ $('.button2').click(function(){
     $('#draggable').toggle();
      });
 
+var countDownDate = new Date("Feb 22, 2019 21:39:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "EXPIRED";
+  }
+}, 1000);
