bae25e6aa502a4cc97d838b842bf2288fe2b11c0
Sujin <ohs710@newschool.edu>
2019-04-08 16:29:12 -0400

    g
    g

--- p5/js/breath.js
+++ p5/js/breath.js
@@ -1,18 +1,7 @@
-var mic;
-function setup(){
-  mic = new p5.AudioIn()
-  mic.start();
-}
-function draw(){
-  background(0);
-  micLevel = mic.getLevel();
-  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
-}
-
 let mic;
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(windowWidth, windowHeight);
 
   // Create an Audio input
   mic = new p5.AudioIn();
