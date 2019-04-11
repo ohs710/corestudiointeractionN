5150004041695563780bcdd9fcc6a5dd4475c16f
Sujin <ohs710@newschool.edu>
2019-04-11 19:23:15 -0400

    add
    add

--- p5/js/breath.js
+++ p5/js/breath.js
@@ -2,9 +2,12 @@ let mic;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  img = loadImage('img/flowmeter.jpg'); // Load the image
+
 
   // Create an Audio input
   mic = new p5.AudioIn();
+
 
   // start the Audio Input.
   // By default, it does not .connect() (to the computer speakers)
@@ -13,20 +16,26 @@ function setup() {
 
 function draw() {
   background(200);
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
 
   // Get the overall volume (between 0 and 1.0)
   let vol = mic.getLevel();
   fill(127);
   stroke(0);
+  background(255);
 
   // Draw an ellipse with height based on volume
   let h = map(vol, 0, 1, height, 0);
+
+  fill(color(0, 200, 400));
+ 
   // create a for loop that iterates over an x value from (0, height) -> (width, height)
   ellipse(width / 2, h - 31, 60, 60);
 }
+
 
 function changeBackground(color) {
    document.body.style.background = color;
 }
 
-window.addEventListener("load",function() { changeBackground('red'); });
+window.addEventListener("load",function() { changeBackground('white'); });
