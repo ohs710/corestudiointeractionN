6e8a6c0aac89bc15e56042bd4bb15da9901e1205
Sujin <ohs710@newschool.edu>
2019-04-11 21:30:08 -0400

    ega
    gea

--- p5/js/breath.js
+++ p5/js/breath.js
@@ -43,18 +43,5 @@ function changeBackground(color) {
 window.addEventListener("load",function() { changeBackground('white'); });
 let timer = 5
 
-function draw() { 
-  background(220);
-  textAlign(CENTER, CENTER);
-  textSize(100);
-  text(timer, width/2, height/2);
-  
-  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-    timer --;
-  }
-  if (timer == 0) {
-    text("GAME OVER", width/2, height*0.7);
-  }
-  
-}
+
 
