d7ce7f8dce2a7823e5b0f039cf204c76db51e1a4
Sujin <ohs710@newschool.edu>
2019-05-01 16:10:51 -0400

    httpstohttp
    https to http

--- apiproject/js/script.js
+++ apiproject/js/script.js
@@ -14,7 +14,7 @@ const getData = event => {
   
    const proxy= 'http://cors-anywhere.herokuapp.com/';
     //const APIKEY ='8ea73c77910f22d9896d5031522bd4b4',
-  const api = `${proxy}http://data.fixer.io/api/latest?access_key=8ea73c77910f22d9896d5031522bd4b4`;
+  const api = `${proxy}https://data.fixer.io/api/latest?access_key=8ea73c77910f22d9896d5031522bd4b4`;
     fetch (api)
     .then(response => response.json())
     .then (data => {
