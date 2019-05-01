c0addb0c23835964369f92cdf03394967164ed62
Sujin <ohs710@newschool.edu>
2019-05-01 16:17:58 -0400

    http
    http and https

--- apiproject/js/script.js
+++ apiproject/js/script.js
@@ -14,11 +14,12 @@ const getData = event => {
   
    const proxy= 'https://cors-anywhere.herokuapp.com/';
     //const APIKEY ='8ea73c77910f22d9896d5031522bd4b4',
-  const api = `${proxy}https://data.fixer.io/api/latest?access_key=8ea73c77910f22d9896d5031522bd4b4`;
+  const api = `${proxy}http://data.fixer.io/api/latest?access_key=8ea73c77910f22d9896d5031522bd4b4`;
     fetch (api)
     .then(response => response.json())
     .then (data => {
       if (data){
+        console.log(data);
         const exchangeRates = data.rates;
         console.log(exchangeRates);
         let euroValue = parseInt(fromAmount.value) / exchangeRates[fromCurrency.value];
